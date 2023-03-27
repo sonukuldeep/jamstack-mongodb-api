d0980d8552fb80d87724564428734ef8031a6d62
sonukuldeep <sonukuldeep@outlook.com>
2023-03-27 12:26:05 +0530

    update

--- pages/api/flutter/index.js
+++ pages/api/flutter/index.js
@@ -1,3 +1,30 @@
+/*
+export default async function handler(req, res) {
+  const fetchOptions = {
+    method: "",
+    headers: {},
+  };
+  const fetchBody = {};
+  const baseUrl = `${process.env.MONGODB_DATA_API_URL}/action`;
+
+  try {
+    switch (req.method) {
+      case "GET":
+        const readData = await fetch('<url>', '<options>');
+        const readDataJson = await readData.json();
+        res.status(  ).json(  );
+        break;
+      default:
+        res.status(405).end();
+        break;
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+}
+*/
+
 export default async function handler(req, res) {
   const fetchOptions = {
     method: "POST",
